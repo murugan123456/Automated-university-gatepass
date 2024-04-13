a7d07eb9a09ca4f0c01013af0ff724d4fc64c88b
MURUGAN MAGESH <131577855+murugan123456@users.noreply.github.com>
2024-04-13 21:49:17 +0530

    Delete Admin-Dash-Board directory

--- Admin-Dash-Board/student-gatepass-register.js
+++
@@ -1,9 +0,0 @@
-const cameraFeed = document.getElementById("cameraFeed");
-
-navigator.mediaDevices.getUserMedia({video:true}).then
-(stream=>{
-    cameraFeed.srcObject = stream;
-})
-.catch(error=>{
-    alert("error accessing camera :",error)
-})
